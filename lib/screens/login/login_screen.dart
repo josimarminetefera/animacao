c1638a6fa5b9cf528c6bb89d3f5ae4e287728e7f
josimarminetefera <josimaminete@gmail.com>
2021-02-09 18:01:41 -0300

    Adicionado stack que irá conter o formulário.

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animacao/screens/login/widget/formulario_widget.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -10,7 +11,36 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            //para que ao clicar no botão a animação cubra a tela toda tem que criar um stack
+            Stack(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 70, bottom: 32),
+                      child: Image.asset(
+                        "images/tickicon.png",
+                        width: 150,
+                        height: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    FormularioWidget(),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/screens/login/widget/formulario_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FormularioWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Form(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
